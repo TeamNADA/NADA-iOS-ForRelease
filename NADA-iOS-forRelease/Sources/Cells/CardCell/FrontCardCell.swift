f8e7cd25722d60e96778ca417ca495b7a3a1682f
hyungyu Kim <ypsophila97@icloud.com>
2021-09-19 23:17:36 +0900

    [#5] 명함 앞면 구현
    ### Descriptions
    - FrontCardCell 구현
    - 뷰컨과 연결
    - 임시 파일 TempCell 삭제
    - swiftlint 수정(function_parameter_count) : 파라미터의 갯수 제한 해제.
    
    ### Related Issue
    close #5

---
+++ NADA-iOS-forRelease/Sources/Cells/CardCell/FrontCardCell.swift
@@ -0,0 +1,66 @@
+//
+//  FrontCardCell.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2021/09/18.
+//
+
+import UIKit
+import VerticalCardSwiper
+
+class FrontCardCell: CardCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var cardNameLabel: UILabel!
+    @IBOutlet weak var detailCardNameLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var mbtiLabel: UILabel!
+    @IBOutlet weak var instagramImageView: UIImageView!
+    @IBOutlet weak var instagramTextLabel: UILabel!
+    @IBOutlet weak var instagramIDLabel: UILabel!
+    @IBOutlet weak var linkImageView: UIImageView!
+    @IBOutlet weak var linkTextLabel: UILabel!
+    @IBOutlet weak var linkIDLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+         setUI()
+    }
+}
+
+// MARK: - Extensions
+
+extension FrontCardCell {
+    private func setUI() {
+        // hidden 에 대한 속성도 여기 쓰자.
+        
+//        instagramImageView.image = UIImage(named: "instagramLogoImg")
+        instagramTextLabel.text = "Instagram"
+    }
+    
+    func setCell(_ cardName: String,
+                 _ detailCardName: String,
+                 _ userName: String,
+                 _ birth: String,
+                 _ mbti: String,
+                 _ instagramID: String,
+                 _ linkImage: String,
+                 _ linkText: String,
+                 _ linkID: String) {
+        self.cardNameLabel.text = cardName
+        self.detailCardNameLabel.text = detailCardName
+        self.userNameLabel.text = userName
+        self.birthLabel.text = birth
+        self.mbtiLabel.text = mbti
+        self.instagramIDLabel.text = instagramID
+        if let linkImage = UIImage(named: linkImage) {
+            self.linkImageView.image = linkImage
+        }
+        self.linkTextLabel.text = linkText
+        self.linkIDLabel.text = linkID
+    }
+}

5b0954036005c6247a6dfe8c045006201318e339
MinjaeLee <2alswo7@khu.ac.kr>
2021-09-30 14:18:56 +0900

    [FEAT] #14 - 카드 리스트 셀 구현

---
+++ NADA-iOS-forRelease/Sources/Cells/CardList/CardListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardListTableViewCell.swift
+//  NADA-iOS-forRelease
+//
+//  Created by 민 on 2021/09/27.
+//
+
+import UIKit
+
+class CardListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NADA-iOS-forRelease/Sources/ViewControllers/CardList/CardListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CardListViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by 민 on 2021/09/27.
+//
+
+import UIKit
+
+class CardListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

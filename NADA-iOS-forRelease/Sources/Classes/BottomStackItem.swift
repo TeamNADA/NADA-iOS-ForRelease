53481e5caa7b86e30c7b65dd965be49a24a962bd
MinjaeLee <2alswo7@khu.ac.kr>
2021-10-08 13:05:59 +0900

    [FEAT] #36 - 탭바 1차 버전 (오류 존재)

---
+++ NADA-iOS-forRelease/Sources/Classes/BottomStackItem.swift
@@ -0,0 +1,8 @@
+//
+//  BottomStackItem.swift
+//  NADA-iOS-forRelease
+//
+//  Created by 민 on 2021/10/08.
+//
+
+import Foundation
---
+++ NADA-iOS-forRelease/Sources/Views/StackItemView.swift
@@ -0,0 +1,20 @@
+//
+//  StackItemView.swift
+//  NADA-iOS-forRelease
+//
+//  Created by 민 on 2021/10/08.
+//
+
+import UIKit
+
+class StackItemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

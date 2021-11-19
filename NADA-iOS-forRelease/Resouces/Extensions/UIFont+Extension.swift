05ab486fea09f5a064023c2fa5429dfb73157a5d
MinjaeLee <2alswo7@khu.ac.kr>
2021-11-19 23:45:10 +0900

    [CHORE] UIFont+Extension 클래스 앞에 @nonobjc 추가

--- NADA-iOS-forRelease/Resouces/Extensions/UIFont+Extension.swift
+++ NADA-iOS-forRelease/Resouces/Extensions/UIFont+Extension.swift
@@ -10,55 +10,55 @@ import UIKit
 
 extension UIFont {
 
-  class var title01: UIFont {
+    @nonobjc class var title01: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Bold", size: 20.0)!
   }
 
-  class var textRegular01: UIFont {
+    @nonobjc class var textRegular01: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Regular", size: 20.0)!
   }
 
-  class var title02: UIFont {
+    @nonobjc class var title02: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Bold", size: 18.0)!
   }
 
-  class var textRegular02: UIFont {
+    @nonobjc class var textRegular02: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Regular", size: 18.0)!
   }
 
-  class var button01: UIFont {
+    @nonobjc class var button01: UIFont {
     return UIFont(name: "NotoSansCJKKR-Bold", size: 16.0)!
   }
 
-  class var textBold01: UIFont {
+    @nonobjc class var textBold01: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Bold", size: 16.0)!
   }
 
-  class var textRegular03: UIFont {
+    @nonobjc class var textRegular03: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Regular", size: 16.0)!
   }
 
-  class var textBold02: UIFont {
+    @nonobjc class var textBold02: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Bold", size: 14.0)!
   }
 
-  class var button02: UIFont {
+    @nonobjc class var button02: UIFont {
     return UIFont(name: "NotoSansCJKKR-Medium", size: 14.0)!
   }
 
-  class var button03: UIFont {
+    @nonobjc class var button03: UIFont {
     return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
   }
 
-  class var textRegular04: UIFont {
+    @nonobjc class var textRegular04: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Regular", size: 14.0)!
   }
 
-  class var textBold03: UIFont {
+    @nonobjc class var textBold03: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Bold", size: 12.0)!
   }
 
-  class var textRegular05: UIFont {
+    @nonobjc class var textRegular05: UIFont {
     return UIFont(name: "SpoqaHanSansNeo-Regular", size: 12.0)!
   }
 

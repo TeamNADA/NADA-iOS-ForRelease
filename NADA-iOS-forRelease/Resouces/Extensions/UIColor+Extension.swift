e3226db75475ac762cb953cdc1a6c299387539b9
MinjaeLee <2alswo7@khu.ac.kr>
2021-11-25 19:40:21 +0900

    [CHORE] #82 - 다크모드 대응 컬러 변경

--- NADA-iOS-forRelease/Resouces/Extensions/UIColor+Extension.swift
+++ NADA-iOS-forRelease/Resouces/Extensions/UIColor+Extension.swift
@@ -32,17 +32,17 @@ extension UIColor {
         }
     }
     
-    @nonobjc class var secondary: UIColor {
+    @nonobjc class var textBox: UIColor {
         if #available(iOS 13, *) {
             return UIColor { (traitCollection: UITraitCollection) -> UIColor in
                 if traitCollection.userInterfaceStyle == .light {
-                    return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
-                } else {
                     return UIColor(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
+                } else {
+                    return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
                 }
             }
         } else {
-            return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
+            return UIColor(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
         }
     }
     
@@ -60,20 +60,6 @@ extension UIColor {
         }
     }
     
-    @nonobjc class var textBox: UIColor {
-        if #available(iOS 13, *) {
-            return UIColor { (traitCollection: UITraitCollection) -> UIColor in
-                if traitCollection.userInterfaceStyle == .light {
-                    return UIColor(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
-                } else {
-                    return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
-                }
-            }
-        } else {
-            return UIColor(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
-        }
-    }
-    
     @nonobjc class var tertiary: UIColor {
         if #available(iOS 13, *) {
             return UIColor { (traitCollection: UITraitCollection) -> UIColor in
@@ -88,17 +74,17 @@ extension UIColor {
         }
     }
     
-    @nonobjc class var background: UIColor {
+    @nonobjc class var secondary: UIColor {
         if #available(iOS 13, *) {
             return UIColor { (traitCollection: UITraitCollection) -> UIColor in
                 if traitCollection.userInterfaceStyle == .light {
-                    return UIColor(white: 1.0, alpha: 1.0)
+                    return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
                 } else {
-                    return UIColor(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
+                    return UIColor(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
                 }
             }
         } else {
-            return UIColor(white: 1.0, alpha: 1.0)
+            return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
         }
     }
     
@@ -119,5 +105,18 @@ extension UIColor {
     @nonobjc class var stateColorError: UIColor {
         return UIColor(red: 243.0 / 255.0, green: 66.0 / 255.0, blue: 53.0 / 255.0, alpha: 1.0)
     }
-    
+
+    @nonobjc class var background: UIColor {
+        if #available(iOS 13, *) {
+            return UIColor { (traitCollection: UITraitCollection) -> UIColor in
+                if traitCollection.userInterfaceStyle == .light {
+                    return UIColor(white: 1.0, alpha: 1.0)
+                } else {
+                    return UIColor(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
+                }
+            }
+        } else {
+            return UIColor(white: 1.0, alpha: 1.0)
+        }
+    }
 }

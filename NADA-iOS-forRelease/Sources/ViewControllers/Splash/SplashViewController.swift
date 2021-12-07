4e9dbc9fe2bd64f93ead7c330ab96764c8785cd9
MinjaeLee <2alswo7@khu.ac.kr>
2021-12-07 19:55:06 +0900

    [FEAT] #114 - 스플래시 뷰 구현 및 폴더링 위치 수정

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/Splash/SplashViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SplashViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by 민 on 2021/12/07.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NADA-iOS-forRelease/Sources/ViewControllers/TabBar/TabBarViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/TabBar/TabBarViewController.swift
@@ -9,19 +9,30 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
     
-    let appearance = UITabBarAppearance()
-
+    // let appearance = UITabBarAppearance()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupStyle()
+        setUI()
     }
     
-    func setupStyle() {
+    func setUI() {
+        self.tabBarController?.tabBar.layer.borderWidth = 1
+        self.tabBarController?.tabBar.layer.borderColor = UIColor.textBox.cgColor
+        // self.tabBarController?.tabBar.clipsToBounds = false
+//        // set tabbar opacity
 //        appearance.configureWithOpaqueBackground()
+//
+//        // remove tabbar border line
 //        appearance.shadowColor = UIColor.clear
+//
+//        // set tabbar background color
+//        appearance.backgroundColor = .background
+//
 //        tabBar.standardAppearance = appearance
 //
+//        // 없으면 iOS15.0 부터는 탭바에 닿기전에는 탭바가 구분되지 않음(이것도 좀 신기해서 이쁘긴함 ㅋㅋ)
 //        if #available(iOS 15.0, *) {
 //            // set tabbar opacity
 //            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
@@ -30,8 +41,8 @@ class TabBarViewController: UITabBarController {
 //        // set tabbar shadow
 //        tabBar.layer.masksToBounds = false
 //        tabBar.layer.shadowColor = UIColor.textBox.cgColor
-//        tabBar.layer.shadowOpacity = 0.3
+//        tabBar.layer.shadowOpacity = 0.5
 //        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
-//        tabBar.layer.shadowRadius = 6
+//        tabBar.layer.shadowRadius = 1
     }
 }

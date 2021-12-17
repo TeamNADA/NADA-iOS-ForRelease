ec7d8ad74437b3ad957c933507e3d1dbbe8b2369
dlwns33 <choi990406@gmail.com>
2021-12-17 17:24:06 +0900

    [FEAT] #164 - bearer 토큰 constant화

---
+++ NADA-iOS-forRelease/Resouces/Constants/Header.swift
@@ -0,0 +1,17 @@
+//
+//  Header.swift
+//  NADA-iOS-forRelease
+//
+//  Created by Yi Joon Choi on 2021/12/17.
+//
+
+import Foundation
+
+extension Const {
+    struct Header {
+//        static let bearerHeader = ["Content-Type": "application/json",
+//                                   "Authorization": "Bearer " + Const.UserDefaults.accessToken]
+        static let bearerHeader = ["Content-Type": "application/json",
+                                   "Authorization": "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuYWRhMiIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE2NDAxMTAyNTB9.qH2fMkrtcVk9GtMEsBEWo0YFvIRTgKPVntBzpeGyyAe5POqnuKf3AtUTJE8yy6gPsAOl0bq8ChCn4RCL0fchpA"]
+    }
+}

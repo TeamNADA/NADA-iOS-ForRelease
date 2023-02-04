7b55b55bb4c6b6b1f098cdee33af0633f9c37bb1
hyun99999 <ypsophila97@icloud.com>
2023-02-05 02:21:57 +0900

    add: Color+Extension (#364)
    - Color 를 확장하여서 위젯의 다크모드를 대응.

--- Widgets/Resource/Extension/Color+Extension.swift
+++ Widgets/Resource/Extension/Color+Extension.swift
@@ -5,4 +5,28 @@
 //  Created by kimhyungyu on 2023/02/04.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static func backgroundColor(for colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .light:
+            return Color(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0)
+        case .dark:
+            return Color(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0, opacity: 0.5)
+        @unknown default:
+            return Color(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0)
+        }
+    }
+    
+    static func userNameColor(for colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .light:
+            return Color(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0)
+        case .dark:
+            return Color(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0)
+        @unknown default:
+            return Color(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0)
+        }
+    }
+}

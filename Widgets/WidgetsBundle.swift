673cb5a92747c07610607d6df33edc78b5c36d5e
hyun99999 <ypsophila97@icloud.com>
2023-02-04 23:34:57 +0900

    chore: Widget Extension 추가 (#364)

---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by kimhyungyu on 2023/02/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsLiveActivity()
+    }
+}

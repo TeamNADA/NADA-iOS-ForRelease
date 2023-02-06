9118bb9a594653f35bb1bc84b0be6f3f9e5ced9f
dlwns33 <choi990406@gmail.com>
2023-02-06 16:36:13 +0900

    add: init homeVC (#357)

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by Yi Joon Choi on 2023/02/06.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

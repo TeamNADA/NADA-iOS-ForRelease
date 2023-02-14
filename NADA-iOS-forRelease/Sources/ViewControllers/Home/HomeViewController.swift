42d6911ce8e472412088c6da8ff13d7d52325866
dlwns33 <choi990406@gmail.com>
2023-02-14 17:31:12 +0900

    feat: init ui components (#357)

--- NADA-iOS-forRelease/Sources/ViewControllers/Home/HomeViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/Home/HomeViewController.swift
@@ -14,24 +14,42 @@ final class HomeViewController: UIViewController {
     // MARK: - Properties
     
     // MARK: - UI Components
-    
+    private let nadaIcon = UIImageView().then {
+        $0.image = UIImage(named: "nadaLogoTxt")
+    }
     private let giveCardImageView = UIImageView().then {
         $0.image = UIImage(named: "cardVertical")
     }
-    
     private let takeCardImageView = UIImageView().then {
         $0.image = UIImage(named: "cardVertical")
     }
-    
     private let aroundMeImageView = UIImageView().then {
         $0.image = UIImage(named: "cardVertical")
+    }
+    private let giveCardLabel = UILabel().then {
+        $0.text = "명함 주기"
+    }
+    private let takeCardLabel = UILabel().then {
+        $0.text = "명함 받기"
+    }
+    private let aroundMeLabel = UILabel().then {
+        $0.text = "내 근처의 명함"
+    }
+    private let giveCardIcon = UIImageView().then {
+        $0.backgroundColor = .blue
+    }
+    private let takeCardIcon = UIImageView().then {
+        $0.backgroundColor = .blue
+    }
+    private let aroundMeIcon = UIImageView().then {
+        $0.backgroundColor = .blue
     }
     
     // MARK: - View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
     }
 
 }
@@ -39,6 +57,11 @@ final class HomeViewController: UIViewController {
 extension HomeViewController {
     
     // MARK: - UI & Layout
+    
+    private func setUI() {
+        self.view.backgroundColor = .white
+        self.navigationController?.navigationBar.isHidden = true
+    }
     
     // MARK: - Methods
 }

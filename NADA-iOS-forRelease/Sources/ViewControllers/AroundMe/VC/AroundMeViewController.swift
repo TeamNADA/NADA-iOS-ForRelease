90595d2f578bed636ed17b004bb1877c66e23f89
dlwns33 <choi990406@gmail.com>
2023-02-28 21:11:51 +0900

    add: init aroundMeVC (#371)

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/AroundMe/VC/AroundMeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AroundMeViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by Yi Joon Choi on 2023/02/28.
+//
+
+import RxSwift
+import RxRelay
+import RxCocoa
+import RxGesture
+import SnapKit
+import Then
+import UIKit
+
+final class AroundMeViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

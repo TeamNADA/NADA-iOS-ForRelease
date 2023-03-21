2177b3d4dda451383d9dddba0a4c2f30ae1704be
hyun99999 <ypsophila97@icloud.com>
2023-03-21 16:50:36 +0900

    feat: Components 추가 및 초기 UI 설정

--- NADA-iOS-forRelease/Sources/ViewControllers/Update/VC/UpdateViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/Update/VC/UpdateViewController.swift
@@ -10,7 +10,72 @@ import UIKit
 import FlexLayout
 
 class UpdateViewController: UIViewController {
+
+    // MARK: - Components
+    
+    private let updateCardImageView = UIImageView()
+    private let cancelButton = UIButton()
+    private let updateContentLabel = UILabel()
+    private let checkBoxButton = UIButton()
+    private let checkLabel = UILabel()
+    private let updateButton = UIButton()
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setUI()
+        addTargets()
+        setLayout()
+    }
+}
+
+// MARK: - Extension
+
+extension UpdateViewController {
+    private func setUI() {
+        updateCardImageView.image = UIImage(named: "imgUpdate")
+        
+        cancelButton.setImage(UIImage(named: "iconClear"), for: .normal)
+        
+        updateContentLabel.numberOfLines = 10
+        updateContentLabel.font = .textRegular04
+        updateContentLabel.textColor = .secondary
+        updateContentLabel.text = ""
+        
+        checkBoxButton.setImage(UIImage(named: "icnCheckboxUnfilled"), for: .normal)
+        checkBoxButton.setImage(UIImage(named: "icnCheckboxFilled"), for: .selected)
+        
+        checkLabel.font = .textRegular05
+        checkLabel.textColor = .tertiary
+        checkLabel.text = "확인했어요!"
+        
+        updateButton.setImage(UIImage(named: "btnMainGoUpdate"), for: .normal)
+    }
+    
+    private func addTargets() {
+        cancelButton.addTarget(self, action: #selector(touchCancelButton), for: .touchUpInside)
+        updateButton.addTarget(self, action: #selector(touchUpdateButton), for: .touchUpInside)
+    }
+    
+    // MARK: - Objc Methods
+    
+    @objc
+    private func touchCancelButton() {
+        // TODO: - 비강제 업데이트는 창 닫기. 강제 업데이트는 앱 종료.
+    }
+    
+    @objc
+    private func touchUpdateButton() {
+        // TODO: - 앱스토어로 연결 혹은 홈으로 연결하는 액션 구현.
+    }
+}
+
+// MARK: - Layout
+
+extension UpdateViewController {
+    private func setLayout() {
+        
     }
 }

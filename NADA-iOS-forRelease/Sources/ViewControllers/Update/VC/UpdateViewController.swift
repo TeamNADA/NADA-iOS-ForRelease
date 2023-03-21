a403174425b4c21bcadf7e03cdbfcb5c60bc2100
hyun99999 <ypsophila97@icloud.com>
2023-03-21 16:47:37 +0900

    add: UpdateViewController 생성 (#381)

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/Update/VC/UpdateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpdateViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/03/21.
+//
+
+import UIKit
+
+import FlexLayout
+
+class UpdateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

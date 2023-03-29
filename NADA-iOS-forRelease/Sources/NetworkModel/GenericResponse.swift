99572b3f4162b49a2519797dd713a68be334bb63
hyun99999 <ypsophila97@icloud.com>
2023-03-29 18:27:14 +0900

    fix: GenericResponse 데이터 모델 수정 (#388)

--- NADA-iOS-forRelease/Sources/NetworkModel/GenericResponse.swift
+++ NADA-iOS-forRelease/Sources/NetworkModel/GenericResponse.swift
@@ -8,20 +8,23 @@
 import Foundation
 
 struct GenericResponse<T: Codable>: Codable {
-    let msg: String
-    let timestamp: String
-    let data: T?
+    let error: NetworkError?
+    let status: Int
+    let result: T?
     
     enum CodingKeys: String, CodingKey {
-        case msg
-        case timestamp
-        case data
+        case error
+        case status
+        case result
     }
+}
+
+struct NetworkError: Codable {
+    let code: Int
+    let message: Int
     
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        msg = (try? values.decode(String.self, forKey: .msg)) ?? ""
-        timestamp = (try? values.decode(String.self, forKey: .timestamp)) ?? ""
-        data = (try? values.decode(T.self, forKey: .data)) ?? nil
+    enum CodingKeys: String, CodingKey {
+        case code
+        case message
     }
 }

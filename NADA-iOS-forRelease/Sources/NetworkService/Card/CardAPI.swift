717c502747bc6e91460f2c1b4d1e743eb236fd53
hyun99999 <ypsophila97@icloud.com>
2023-03-30 11:26:48 +0900

    feat: URL 주소를 전달하는 API 메서드 구현 (#390)

--- NADA-iOS-forRelease/Sources/NetworkService/Card/CardAPI.swift
+++ NADA-iOS-forRelease/Sources/NetworkService/Card/CardAPI.swift
@@ -109,6 +109,20 @@ public class CardAPI {
         }
     }
     
+    func cardImageUpload(image: UIImage, completion: @escaping (NetworkResult<Any>) -> Void) {
+        cardProvider.request(.imageUpload(image: image)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data)
+                
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
     private func judgeCardDetailFetchStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         
         let decoder = JSONDecoder()

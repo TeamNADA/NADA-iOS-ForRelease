6763fcbf93cc83b71c96bc4b669b0856161738fe
hyun99999 <ypsophila97@icloud.com>
2023-04-05 16:01:51 +0900

    add: 업데이트 내용 조회 / 사용자 업데이트 정보 조회 API 를 위한 파일 생성 (#397)

---
+++ NADA-iOS-forRelease/Sources/NetworkModel/Update/UpdateNote.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateNote.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/04/05.
+//
+
+import Foundation
---
+++ NADA-iOS-forRelease/Sources/NetworkService/Update/UpdateService.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateService.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/04/05.
+//
+
+import Foundation

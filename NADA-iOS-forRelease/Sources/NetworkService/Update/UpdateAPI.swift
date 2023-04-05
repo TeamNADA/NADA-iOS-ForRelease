ddb88f32eca12ef40959a7c9a40a2d6cbeb80bd1
hyun99999 <ypsophila97@icloud.com>
2023-04-05 16:44:55 +0900

    feat: API 구현 (#397)
    - final class 로 구현
        - 상속할 수 없도록 구현하여 static dispatch 로 실행되도록 설정.
    - judgeStatus() 메서드에서 타입을 정할 수 있도록해서 타입별로 여러개 생성하던 것에서 하나의 메서드를 사용하도록 리펙토링.

--- NADA-iOS-forRelease/Sources/NetworkService/Update/UpdateAPI.swift
+++ NADA-iOS-forRelease/Sources/NetworkService/Update/UpdateAPI.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+import Moya
+
+public final class UpdateAPI {
+    static let shared = UpdateAPI()
+    private var updateProvider = MoyaProvider<UpdateService>(plugins: [MoyaLoggerPlugin()])
+    
+    private init() { }
+    
+    func updateNoteFetch(completion: @escaping(NetworkResult<Any>) -> Void) {
+        updateProvider.request(.updateNoteFetch) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeStatus(by: statusCode, data: data, type: UpdateNote.self)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    func updateUserInfoFetch(completion: @escaping (NetworkResult<Any>) -> Void) {
+        updateProvider.request(.updateUserInfoFetch) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeStatus(by: statusCode, data: data, type: UpdateUserInfo.self)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    // MARK: - judgeStatus methods
+    
+    private func judgeStatus<T: Codable>(by statusCode: Int, data: Data, type: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<T>.self, from: data)
+        else { return .pathErr }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400..<500:
+            return .requestErr(decodedData.error?.message ?? "error message")
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}

530ea6ef36e1882081102bfad44ea4e6f3b19138
dlwns33 <choi990406@gmail.com>
2023-04-11 20:25:54 +0900

    add: add receiveBottomVC (#403)

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/BottomSheet/ReceiveCardBottomSheetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ReceiveCardBottomSheetViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by Yi Joon Choi on 2023/04/11.
+//
+
+import UIKit
+
+class ReceiveCardBottomSheetViewController: CommonBottomSheetViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - View Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
+
+extension ReceiveCardBottomSheetViewController {
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+}

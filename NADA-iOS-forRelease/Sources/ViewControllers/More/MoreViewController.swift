9a83c633c157fd7243c425232658b2a147647c40
hyun99999 <ypsophila97@icloud.com>
2023-04-25 00:28:44 +0900

    refactor: 와일드 카드 사용 (#460)

--- NADA-iOS-forRelease/Sources/ViewControllers/More/MoreViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/More/MoreViewController.swift
@@ -135,7 +135,7 @@ extension MoreViewController {
         makeOKCancelAlert(title: "", message: "정말 탈퇴하시겠습니까?\n앱 내 정보가 모두 삭제되며, 이후 복구는 불가합니다.", okAction: { [weak self ]_ in
             self?.deleteUserWithAPI {
                 self?.makeOKAlert(title: "", message: "나다를 이용해주셔서 감사합니다.\n다음에 또 뵈어요! 🥹") { _ in
-                    if let acToken = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) {
+                    if let _ = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) {
                         self?.defaults.removeObject(forKey: Const.UserDefaultsKey.accessToken)
 //                        self.defaults.removeObject(forKey: Const.UserDefaultsKey.refreshToken)
                         self?.defaults.removeObject(forKey: Const.UserDefaultsKey.darkModeState)

ce8f342351eece75e3a000f788f6e923d040f09e
hyun99999 <ypsophila97@icloud.com>
2023-04-24 19:26:58 +0900

    feat: 명함 생성 시 cardType 변수 사용 (#450)

--- NADA-iOS-forRelease/Sources/ViewControllers/CardCreation/VC/CardCreationViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/CardCreation/VC/CardCreationViewController.swift
@@ -87,7 +87,7 @@ class CardCreationViewController: UIViewController {
         nextVC.backCardDataModel = backCard
         nextVC.cardBackgroundImage = backgroundImage
         nextVC.tasteInfo = tasteInfo
-        nextVC.cardType = .basic
+        nextVC.cardType = cardType
         navigationController?.pushViewController(nextVC, animated: true)
     }
 }

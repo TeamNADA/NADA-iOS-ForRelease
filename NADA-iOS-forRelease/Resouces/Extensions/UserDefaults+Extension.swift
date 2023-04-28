4d08948610da0afdf9cf519299cdcea45e6639cc
hyun99999 <ypsophila97@icloud.com>
2023-04-28 18:22:24 +0900

    add: UserDefaults+Extension 생성
    - app group 을 위한 UserDefaults 생성

---
+++ NADA-iOS-forRelease/Resouces/Extensions/UserDefaults+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaults+Extension.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/04/28.
+//
+
+import Foundation
+
+extension UserDefaults {
+    static var appGroup = UserDefaults(suiteName: "group.NADA-iOS-forRelease")!
+}

8ebce04aabbe866417a7191fa11267663e0382df
hyun99999 <ypsophila97@icloud.com>
2023-04-30 23:41:12 +0900

    feat: 명함 조회 API 연결 (#463)

--- IntentsExtension/IntentHandler.swift
+++ IntentsExtension/IntentHandler.swift
@@ -38,3 +38,43 @@ extension IntentHandler: MyCardIntentHandling {
         return myCard
     }
 }
+
+// MARK: - Newtwork
+
+enum WidgetError: Error {
+    case networkFail(status: Int, code: String, message: String)
+    case decodeFail(status: Int)
+    case error(status: Int, error: Error)
+}
+
+extension IntentHandler {
+    func cardListFetchWithAPI(completion: @escaping (Result<GenericResponse<[Card]>?, Error>) -> Void) {
+        guard let url = URL(string: "http://3.35.107.3:8080/api/v1/card") else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.addValue("Authorization", forHTTPHeaderField: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwfQ.rq__Rzvunpxq3paeo2fK4i_oeupyDlHp3q1RW6uSHSQ")
+//        print("😀", UserDefaults.appGroup.string(forKey: "accessToken") ?? "")
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let status = (response as? HTTPURLResponse)?.statusCode else { return }
+            
+            if let error = error {
+                completion(.failure(WidgetError.error(status: status, error: error)))
+            } else {
+                if let data {
+                    let result = try? JSONDecoder().decode(GenericResponse<[Card]>.self, from: data)
+                    if status != 200 {
+                        completion(.failure(WidgetError.networkFail(status: status,
+                                                                    code: result?.code ?? "none code",
+                                                                    message: result?.message ?? "none message")))
+                        print("😃", result?.status)
+                    } else {
+                        completion(.success(result))
+                    }
+                } else {
+                    completion(.failure(WidgetError.decodeFail(status: status)))
+                }
+            }
+        }.resume()
+    }
+}

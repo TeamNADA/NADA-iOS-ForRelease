7fb64a3f7387baf710fb779b8665488d2a9b35a2
hyun99999 <ypsophila97@icloud.com>
2023-05-02 19:51:48 +0900

    feat: IntentsHandler 에서 서버통신을 통한 명함 목록 전달 (#463)

--- IntentsExtension/IntentHandler.swift
+++ IntentsExtension/IntentHandler.swift
@@ -9,29 +9,61 @@ import Intents
 
 class IntentHandler: INExtension {
     
+    // MARK: - Properties
+    
+    var cardItems: [Card]?
+    
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
+        
+        cardListFetchWithAPI { [weak self] result in
+            switch result {
+            case .success(let result):
+                if let result {
+                    self?.cardItems = result.data
+                    print(self?.cardItems)
+                }
+            case .failure(let err):
+                print(err)
+            }
+        }
         
         return self
     }
 }
 
 extension IntentHandler: MyCardIntentHandling {
+    // 내 명함 목록 선택할 때 호출.
     func provideMyCardOptionsCollection(for intent: MyCardIntent, with completion: @escaping (INObjectCollection<MyCard>?, Error?) -> Void) {
-        if let cardItems {
-            let myCards = cardItems.map { card in
-                let myCard = MyCard(identifier: card.cardUUID, display: card.cardName)
-                
-                return myCard
+        cardListFetchWithAPI { [weak self] result in
+            switch result {
+            case .success(let result):
+                if let result {
+                    self?.cardItems = result.data
+                    
+                    if let cardItems = self?.cardItems {
+                        let myCards = cardItems.map { card in
+                            let myCard = MyCard(identifier: card.cardUUID, display: card.cardName)
+                            myCard.userName = card.userName
+                            myCard.cardImage = card.cardImage
+                            
+                            return myCard
+                        }
+                        let collection = INObjectCollection(items: myCards)
+                        completion(collection, nil)
+                    }
+                }
+            case .failure(let err):
+                print(err)
             }
-            let collection = INObjectCollection(items: myCards)
-            completion(collection, nil)
         }
     }
     
+    // 위젯 편집할때 호출. 기본값 설정.
     func defaultMyCard(for intent: MyCardIntent) -> MyCard? {
         var myCard: MyCard?
+        
         if let cardItems {
             myCard = MyCard(identifier: cardItems[0].cardUUID, display: cardItems[0].cardName)
         }
@@ -53,8 +85,8 @@ extension IntentHandler {
         guard let url = URL(string: "http://3.35.107.3:8080/api/v1/card") else { return }
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
-        urlRequest.addValue("Authorization", forHTTPHeaderField: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwfQ.rq__Rzvunpxq3paeo2fK4i_oeupyDlHp3q1RW6uSHSQ")
-//        print("😀", UserDefaults.appGroup.string(forKey: "accessToken") ?? "")
+        urlRequest.addValue("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwfQ.rq__Rzvunpxq3paeo2fK4i_oeupyDlHp3q1RW6uSHSQ", forHTTPHeaderField: "Authorization")
+        print("😀", UserDefaults.appGroup.string(forKey: "accessToken") ?? "")
         
         URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             guard let status = (response as? HTTPURLResponse)?.statusCode else { return }
@@ -64,16 +96,18 @@ extension IntentHandler {
             } else {
                 if let data {
                     let result = try? JSONDecoder().decode(GenericResponse<[Card]>.self, from: data)
+                    
                     if status != 200 {
                         completion(.failure(WidgetError.networkFail(status: status,
                                                                     code: result?.code ?? "none code",
                                                                     message: result?.message ?? "none message")))
-                        print("😃", result?.status)
                     } else {
-                        completion(.success(result))
+                        if let result {
+                            completion(.success(result))
+                        } else {
+                            completion(.failure(WidgetError.decodeFail(status: status)))
+                        }
                     }
-                } else {
-                    completion(.failure(WidgetError.decodeFail(status: status)))
                 }
             }
         }.resume()

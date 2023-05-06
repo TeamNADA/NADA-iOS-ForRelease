22c836fcd862f9ed7b3dd1f503dfbabcbce067d9
hyun99999 <ypsophila97@icloud.com>
2023-05-06 21:36:45 +0900

    add: Font+Extension 추가 (#488)

---
+++ Widgets/Resource/Extension/Font+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Font+Extension.swift
+//  WidgetsExtension
+//
+//  Created by kimhyungyu on 2023/05/05.
+//
+
+import SwiftUI
+
+extension Font {
+    static let cardName = Font.custom("SpoqaHanSansNeo-Regular", size: 15)
+    static let userName = Font.custom("SpoqaHanSansNeo-Bold", size: 15)
+}

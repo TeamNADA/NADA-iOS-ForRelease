7936c07d40f11b7eac221814a69d0ef2de8397c0
dlwns33 <choi990406@gmail.com>
2023-05-08 15:59:39 +0900

    add: 궁합 로티 및 UI component (#490)

--- NADA-iOS-forRelease/Resouces/Constants/Lottie.swift
+++ NADA-iOS-forRelease/Resouces/Constants/Lottie.swift
@@ -14,5 +14,10 @@ extension Const {
         static let nearby = "nearby.json"
         static let onboarding01 = "onboarding01.json"
         static let onboarding02 = "onboarding02.json"
+        static let nadaHarmonyLottie020 = "gunghap_0_20.json"
+        static let nadaHarmonyLottie2140 = "gunghap_21_40.json"
+        static let nadaHarmonyLottie4160 = "gunghap_41_60.json"
+        static let nadaHarmonyLottie6180 = "gunghap_61_80.json"
+        static let nadaHarmonyLottie81100 = "gunghap_81_100.json"
     }
 }
--- NADA-iOS-forRelease/Sources/ViewControllers/CardDetail/NewCardHarmonyViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/CardDetail/NewCardHarmonyViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import Kingfisher
+import Lottie
 import NVActivityIndicatorView
 import RxSwift
 import RxRelay
@@ -22,17 +23,103 @@ class NewCardHarmonyViewController: UIViewController {
     
     // MARK: - Components
     
+    private let xButton = UIButton().then {
+        $0.setImage(UIImage(named: "iconClear"), for: .normal)
+        $0.addTarget(self, action: #selector(dismissPopUp), for: .touchUpInside)
+    }
     
+    private let popUpView = UIView().then {
+        $0.backgroundColor = .background
+        $0.layer.cornerRadius = 22
+    }
+    
+    private let mbtiIcon = UIImageView().then {
+        $0.image = UIImage(named: "iconMbti")
+    }
+    
+    private let mbtiTitleLabel = UILabel().then {
+        $0.font = .textRegular03
+        $0.text = "MBTI 점수"
+        $0.textColor = .secondary
+        $0.sizeToFit()
+    }
+    
+    private let mbtiScoreLabel = UILabel().then {
+        $0.font = .textBold01
+        $0.text = "NN점"
+        $0.textColor = .secondary
+        $0.sizeToFit()
+    }
+    
+    private let dividerLine = UIView().then {
+        $0.backgroundColor = .quaternary
+    }
+    
+    private let starIcon = UIImageView().then {
+        $0.image = UIImage(named: "iconStar")
+    }
+    
+    private let starTitleLabel = UILabel().then {
+        $0.font = .textRegular03
+        $0.text = "별자리 점수"
+        $0.textColor = .secondary
+        $0.sizeToFit()
+    }
+    
+    private let starScoreLabel = UILabel().then {
+        $0.font = .textBold01
+        $0.text = "NN점"
+        $0.textColor = .secondary
+        $0.sizeToFit()
+    }
+    
+    private let nadaDescLabel = UILabel().then {
+        $0.font = .button02
+        $0.text = "NADA만의 랜덤 점수까지 더하면!"
+        $0.textColor = .tertiary
+        $0.sizeToFit()
+    }
+    
+    private let nadaHarmonyLottie020 = LottieAnimationView(name: Const.Lottie.onboarding01)
+    private let nadaHarmonyLottie2140 = LottieAnimationView(name: Const.Lottie.nadaHarmonyLottie2140)
+    private let nadaHarmonyLottie4160 = LottieAnimationView(name: Const.Lottie.nadaHarmonyLottie4160)
+    private let nadaHarmonyLottie6180 = LottieAnimationView(name: Const.Lottie.nadaHarmonyLottie6180)
+    private let nadaHarmonyLottie81100 = LottieAnimationView(name: Const.Lottie.nadaHarmonyLottie81100)
+    
+    // MARK: - View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
+        setLayout()
     }
-
 }
 
 // MARK: - Extensions
 
 extension NewCardHarmonyViewController {
+    
+    // MARK: UI & Layout
+    
+    private func setUI() {
+        nadaHarmonyLottie020.loopMode = .playOnce
+        nadaHarmonyLottie2140.loopMode = .playOnce
+        nadaHarmonyLottie4160.loopMode = .playOnce
+        nadaHarmonyLottie6180.loopMode = .playOnce
+        nadaHarmonyLottie81100.loopMode = .playOnce
+        
+//        onboardingFirstLottieView.play()
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - @objc
+    
+    @objc
+    private func dismissPopUp() {
+        self.dismiss(animated: false)
+    }
     
 }

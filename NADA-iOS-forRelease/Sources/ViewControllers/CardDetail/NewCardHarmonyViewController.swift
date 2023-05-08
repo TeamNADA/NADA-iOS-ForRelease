fcde3a7739b2697eb8c09fb9a65dc8bf340282ba
dlwns33 <choi990406@gmail.com>
2023-05-08 14:11:02 +0900

    add: 궁합 뷰 추가 (#490)

---
+++ NADA-iOS-forRelease/Sources/ViewControllers/CardDetail/NewCardHarmonyViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewCardHarmonyViewController.swift
+//  NADA-iOS-forRelease
+//
+//  Created by Yi Joon Choi on 2023/05/08.
+//
+
+import UIKit
+
+import Kingfisher
+import NVActivityIndicatorView
+import RxSwift
+import RxRelay
+import RxCocoa
+import RxGesture
+import SnapKit
+import Then
+
+class NewCardHarmonyViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Components
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+// MARK: - Extensions
+
+extension NewCardHarmonyViewController {
+    
+}

0ecbc747f5dbf6ef894ce433532c05c2aee6a35e
hyun99999 <ypsophila97@icloud.com>
2023-05-15 14:02:32 +0900

    fix: 네트워크 실패에 명함 공유 바텀시트 띄우지 않도록 하기 (#529)
    - 완전하게 공유 바텀시트 혹은 카메라가 뜬 다음에 openQRCodeWidget 과 openMyCardWidget, widgetCardUUID userdefault 값을 지워주었습니다. 리펙토링.

--- NADA-iOS-forRelease/Sources/ViewControllers/Home/VC/HomeViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/Home/VC/HomeViewController.swift
@@ -185,38 +185,32 @@ extension HomeViewController {
                         self?.presentToUpdateVC(with: updateNote)
                         UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.dynamicLinkCardUUID)
                     } else {
-                        if let dynamicLinkCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.dynamicLinkCardUUID) {
-                            self?.checkDynamicLink(dynamicLinkCardUUID)
-                        }
-                        
                         if UserDefaults.standard.bool(forKey: Const.UserDefaultsKey.openQRCodeWidget) {
                             self?.presentQRScanVC()
-                            UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openQRCodeWidget)
                         }
                         
                         if UserDefaults.standard.bool(forKey: Const.UserDefaultsKey.openMyCardWidget),
                            let widgetCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.widgetCardUUID) {
                             self?.presentCardShareBottomSheetVC(with: widgetCardUUID)
-                            UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openMyCardWidget)
-                            UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.widgetCardUUID)
+                        }
+                        
+                        if let dynamicLinkCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.dynamicLinkCardUUID) {
+                            self?.checkDynamicLink(dynamicLinkCardUUID)
                         }
                     }
                 }
             } else {
-                if let dynamicLinkCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.dynamicLinkCardUUID) {
-                    self?.checkDynamicLink(dynamicLinkCardUUID)
-                }
-                
                 if UserDefaults.standard.bool(forKey: Const.UserDefaultsKey.openQRCodeWidget) {
                     self?.presentQRScanVC()
-                    UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openQRCodeWidget)
                 }
                 
                 if UserDefaults.standard.bool(forKey: Const.UserDefaultsKey.openMyCardWidget),
                    let widgetCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.widgetCardUUID) {
                     self?.presentCardShareBottomSheetVC(with: widgetCardUUID)
-                    UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openMyCardWidget)
-                    UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.widgetCardUUID)
+                }
+                
+                if let dynamicLinkCardUUID = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.dynamicLinkCardUUID) {
+                    self?.checkDynamicLink(dynamicLinkCardUUID)
                 }
             }
         }
@@ -263,21 +257,27 @@ extension HomeViewController {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
         case .denied:
             makeOKCancelAlert(title: "카메라 권한이 허용되어 있지 않아요.",
-                        message: "QR코드 인식을 위해 카메라 권한이 필요합니다. 앱 설정으로 이동해 허용해 주세요.",
-                        okAction: { _ in UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)},
-                        cancelAction: nil,
-                        completion: nil)
+                              message: "QR코드 인식을 위해 카메라 권한이 필요합니다. 앱 설정으로 이동해 허용해 주세요.",
+                              okAction: { _ in UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)},
+                              cancelAction: nil,
+                              completion: {
+                UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openQRCodeWidget)
+            })
         case .authorized:
             guard let nextVC = UIStoryboard.init(name: Const.Storyboard.Name.qrScan, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.qrScanViewController) as? QRScanViewController else { return }
             nextVC.modalPresentationStyle = .overFullScreen
-            self.present(nextVC, animated: true, completion: nil)
+            self.present(nextVC, animated: true) {
+                UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openQRCodeWidget)
+            }
         case .notDetermined:
             AVCaptureDevice.requestAccess(for: .video) { granted in
                 if granted {
                     DispatchQueue.main.async {
                         guard let nextVC = UIStoryboard.init(name: Const.Storyboard.Name.qrScan, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.qrScanViewController) as? QRScanViewController else { return }
                         nextVC.modalPresentationStyle = .overFullScreen
-                        self.present(nextVC, animated: true, completion: nil)
+                        self.present(nextVC, animated: true) {
+                            UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openQRCodeWidget)
+                        }
                     }
                 }
             }
@@ -297,7 +297,10 @@ extension HomeViewController {
                 nextVC.modalPresentationStyle = .overFullScreen
                 nextVC.cardDataModel = cardDataModel
                 
-                self?.present(nextVC, animated: true)
+                self?.present(nextVC, animated: true) {
+                    UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.openMyCardWidget)
+                    UserDefaults.standard.removeObject(forKey: Const.UserDefaultsKey.widgetCardUUID)
+                }
             }
         }
     }

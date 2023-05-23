87c774a9a370b12dd1045c03d22d395b79edf398
hyun99999 <ypsophila97@icloud.com>
2023-05-23 14:36:10 +0900

    feat: 명함 리스트 트래킹 (#542)
    - 트래킹 스크린 이름 설정.

--- NADA-iOS-forRelease/Resouces/Tracking/Event.swift
+++ NADA-iOS-forRelease/Resouces/Tracking/Event.swift
@@ -13,18 +13,21 @@ extension Tracking {
         static let touchOnboardingStart = "A2 온보딩_시작"
         static let touchKakaoLogin = "A3 로그인_카카오"
         static let touchAppleLogin = "A3 로그인_애플"
+        
         static let touchCreateCard = "C1 내명함_추가"
         static let touchCardList = "C1 내명함_목록"
         static let scrollMyCard = "C1 내명함_imp_"
         static let touchCopyCardID = "C2 명함공유 모달_복사"
+        
         static let touchSaveCardAsImage = "C2 명함공유 모달_저장"
         static let touchCardShareOn = "C2 명함공유 모달_내근처_ON"
         static let touchCardShareOff = "C2 명함공유 모달_내근처_OFF"
+        
         static let touchCreateCardCategoryBack = "C3 명함만들기_뒤로가기"
         static let touchBasicCategory = "C3 명함만들기_기본"
         static let touchFanCategory = "C3 명함만들기_덕질"
         static let touchCompanyCategory = "C3 명함만들기_직장"
-        static let touchCardListPin = "C9 명함리스트_핀"
+        
         static let touchExitCreateBasicCard = "C4 명함만들기_기본_종료"
         static let touchFrontCreateBasicCard = "C4 명함만들기_기본_앞면"
         static let touchBackCreateBasicCard = "C4 명함만들기_기본_뒷면"
@@ -32,8 +35,10 @@ extension Tracking {
         static let touchBasicDefaultImage = "C4 명함만들기_기본_이미지"
         static let touchBasicTasteInfo = "C4 명함만들기_기본_"
         static let touchBasicCardPreview = "C4 명함만들기_기본_미리보기"
-        static let touchPreviewBackFromBasic = "C5 명함만들기_기본_미리보기_뒤로가기"
+        
+        static let touchBackFromBasicPreview = "C5 명함만들기_기본_미리보기_뒤로가기"
         static let touchPreviewBasicComplete = "C5 명함만들기_기본_미리보기_완료"
+        
         static let touchExitCreateFanCard = "C6 명함만들기_덕질_종료"
         static let touchFrontCreateFanCard = "C6 명함만들기_덕질_앞면"
         static let touchBackCreateFanCard = "C6 명함만들기_덕질_뒷면"
@@ -41,6 +46,7 @@ extension Tracking {
         static let touchFanDefaultImage = "C6 명함만들기_덕질_이미지"
         static let touchFanTasteInfo = "C6 명함만들기_덕질_"
         static let touchFanCardPreview = "C6 명함만들기_덕질_미리보기"
+        
         static let touchBackFromFanPreview = "C7 명함만들기_덕질_미리보기_뒤로가기"
         static let touchPreviewFanComplete = "C7 명함만들기_덕질_미리보기_완료"
         
@@ -55,6 +61,7 @@ extension Tracking {
         static let touchBackFromCompanyPreview = "C9 명함만들기_직장_미리보기_뒤로가기"
         static let touchPreviewCompanyComplete = "C9 명함만들기_덕질_미리보기_완료"
         
+        static let touchCardListPin = "C10 명함리스트_핀"
         static let touchDarkmode = "E1 설정_다크모드"
         static let touchPrivacyPolicy = "E1 설정_개인정보"
         static let touchTermsOfUse = "E1 설정_이용약관"

240983ec6fadd13bee746dd5c16ac1cde53c19b8
hyun99999 <ypsophila97@icloud.com>
2023-05-23 15:28:42 +0900

    add: Event 빈 줄 추가 (#542)

--- NADA-iOS-forRelease/Resouces/Tracking/Event.swift
+++ NADA-iOS-forRelease/Resouces/Tracking/Event.swift
@@ -11,6 +11,7 @@ extension Tracking {
     struct Event {
         private init() { }
         static let touchOnboardingStart = "A2 온보딩_시작"
+        
         static let touchKakaoLogin = "A3 로그인_카카오"
         static let touchAppleLogin = "A3 로그인_애플"
         

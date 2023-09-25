b1588263c9f19a47e7b96d285bb8ecd66ebe4e5b
hyun99999 <ypsophila97@icloud.com>
2023-09-25 23:00:37 +0900

    add: Tag data model 추가 (#607)

---
+++ NADA-iOS-forRelease/Sources/NetworkModel/Tag/Tag.swift
@@ -0,0 +1,21 @@
+//
+//  Tag.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/09/25.
+//
+
+import Foundation
+
+struct Tag: Codable {
+    let adjective: String
+    let cardTagId: Int
+    let darkB: Int
+    let darkG: Int
+    let darkR: Int
+    let icon: String
+    let noun: String
+    let whiteB: Int
+    let whiteG: Int
+    let whiteR: Int
+}

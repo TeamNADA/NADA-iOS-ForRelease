166cdb288a804e19e89114aab9c51965f15ae984
hyun99999 <ypsophila97@icloud.com>
2023-09-26 01:04:11 +0900

    add: 태그 관련 데이터 모델 추가 (#609)

---
+++ NADA-iOS-forRelease/Sources/NetworkModel/Tag/CreateTagRequest.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTagRequest.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/09/26.
+//
+
+import Foundation
+
+struct CreatTagRequest: Codable {
+    let adjective: String
+    let cardUUID: String
+    let icon: String
+    let noun: String
+    
+    enum CodingKeys: String, CodingKey {
+        case cardUUID = "cardUuid"
+        case adjective, icon, noun
+    }
+}
--- NADA-iOS-forRelease/Sources/NetworkModel/Tag/ReceivedTag.swift
+++ NADA-iOS-forRelease/Sources/NetworkModel/Tag/ReceivedTag.swift
@@ -1,17 +1,20 @@
 //
-//  Tag.swift
+//  ReceivedTag.swift
 //  NADA-iOS-forRelease
 //
-//  Created by kimhyungyu on 2023/09/26.
+//  Created by kimhyungyu on 2023/09/25.
 //
 
 import Foundation
 
-struct Tag: Codable {
+struct ReceivedTag: Codable {
+    let adjective: String
+    let cardTagId: Int
     let darkB: Int
     let darkG: Int
     let darkR: Int
     let icon: String
+    let noun: String
     let whiteB: Int
     let whiteG: Int
     let whiteR: Int

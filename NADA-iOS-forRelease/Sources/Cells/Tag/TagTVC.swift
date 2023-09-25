33e62c8e8fd41c12976fca8698b3fb5b83604bbf
hyun99999 <ypsophila97@icloud.com>
2023-09-25 23:03:06 +0900

    feat: layout 설정 (#607)

--- NADA-iOS-forRelease/Sources/Cells/Tag/TagTVC.swift
+++ NADA-iOS-forRelease/Sources/Cells/Tag/TagTVC.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
+import SnapKit
+
 class TagTVC: UITableViewCell {
+    
+    // MARK: - Components
+    
+    private let bgView = UIView()
+    private let tagImageView = UIImageView()
+    private let tagLabel = UILabel()
+    private let selectedBgView = UIView()
+    private let checkImageView = UIImageView()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,5 +30,36 @@ class TagTVC: UITableViewCell {
 
     override func prepareForReuse() {
         super.prepareForReuse()
+    }
+}
+// MARK: - extension
+
+extension TagTVC {
+    private func setLayout() {
+        contentView.addSubviews([bgView, tagImageView, tagLabel, selectedBgView])
+        
+        bgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        tagImageView.snp.makeConstraints { make in
+            make.top.left.bottom.equalToSuperview().inset(10)
+            make.width.equalTo(tagImageView.snp.height)
+        }
+        
+        tagLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        selectedBgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        selectedBgView.addSubview(checkImageView)
+        
+        checkImageView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(14)
+            make.width.equalTo(checkImageView.snp.height)
+        }
     }
 }

be841657c9ffd7308530dc3b11eabbc0fb6801ed
hyun99999 <ypsophila97@icloud.com>
2023-09-25 15:18:56 +0900

    feat: iOS 16미만 버전인 경우 medium으로 설정 (#602)

--- NADA-iOS-forRelease/Sources/ViewControllers/Main/FrontViewController.swift
+++ NADA-iOS-forRelease/Sources/ViewControllers/Main/FrontViewController.swift
@@ -174,6 +174,8 @@ extension FrontViewController {
         if let cardUUID = notification.object as? String {
             let tagSheet = FetchTagSheetVC()
             
+            // detent custom 생성
+            if #available(iOS 16.0, *) {
                 let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
                 let safeAreaBottom = windowScene?.windows.first?.safeAreaInsets.bottom ?? 0
                 let detentIdentifier = UISheetPresentationController.Detent.Identifier("tagSheet")
@@ -183,6 +185,16 @@ extension FrontViewController {
                 
                 if let sheet = tagSheet.sheetPresentationController {
                     sheet.detents = [customDetent, .large()] // detent 설정
+                    sheet.preferredCornerRadius = 30 // 둥글기 수정
+                }
+            } else {
+                if let sheet = tagSheet.sheetPresentationController {
+                    sheet.detents = [.medium(), .large()] // detent 설정
+                    // sheet.prefersGrabberVisible = false // 기본값
+                    // grabber 이미지로 대체하였음.
+                    
+                    // 스크롤 상황에서 최대 detent까지 확장하는 여부 결정.
+                    // sheet.prefersScrollingExpandsWhenScrolledToEdge = true // 기본값
                     sheet.preferredCornerRadius = 30 // 둥글기 수정
                 }
             }

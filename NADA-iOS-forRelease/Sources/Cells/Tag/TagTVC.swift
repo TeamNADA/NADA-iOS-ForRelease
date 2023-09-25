b7048815ac78c4a0ed5c15e4728536b738546a17
hyun99999 <ypsophila97@icloud.com>
2023-09-25 23:02:18 +0900

    add: TagTVC 추가 (#607)

---
+++ NADA-iOS-forRelease/Sources/Cells/Tag/TagTVC.swift
@@ -0,0 +1,24 @@
+//
+//  TagTVC.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/09/25.
+//
+
+import UIKit
+
+class TagTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}

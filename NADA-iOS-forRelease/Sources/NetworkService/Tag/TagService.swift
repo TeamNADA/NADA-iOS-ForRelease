70f2ebc3e4f3a2b0213c3dd16aa4901221e85099
hyun99999 <ypsophila97@icloud.com>
2023-09-26 10:58:58 +0900

    add: TagService 생성 (#609)

---
+++ NADA-iOS-forRelease/Sources/NetworkService/Tag/TagService.swift
@@ -0,0 +1,60 @@
+//
+//  TagService.swift
+//  NADA-iOS-forRelease
+//
+//  Created by kimhyungyu on 2023/09/26.
+//
+
+import Foundation
+
+import Moya
+
+enum TagService {
+    case tagCreation(request: CreateTagRequest)
+    case tagFetch
+    case receivedTagFetch(cardUUID: String)
+}
+
+extension TagService: TargetType {
+    var baseURL: URL {
+        return URL(string: Const.URL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .tagCreation:
+            return "/card/tag"
+        case .tagFetch:
+            return "/tag/image"
+        case .receivedTagFetch(let cardUUID):
+            return "/card/tag/card/\(cardUUID)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .tagCreation:
+            return .post
+        case .tagFetch, .receivedTagFetch:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .tagCreation(let request):
+            return .requestJSONEncodable(request)
+        case .tagFetch, .receivedTagFetch:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .tagCreation:
+            return Const.Header.basicHeader()
+        case .tagFetch, .receivedTagFetch:
+            return Const.Header.bearerHeader()
+        }
+    }
+}

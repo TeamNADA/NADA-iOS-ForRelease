9de2c458eadeb3de00f2272f6be0bfa827637836
hyun99999 <ypsophila97@icloud.com>
2023-10-04 22:46:20 +0900

    add: NetworkResult 추가 (#609)
    - 성공시에만 제네릭을 활용하여 데이터 모델을 전달하도록 설정한 network result 추가.

--- NADA-iOS-forRelease/Sources/NetworkModel/NetworkResult.swift
+++ NADA-iOS-forRelease/Sources/NetworkModel/NetworkResult.swift
@@ -14,3 +14,11 @@ public enum NetworkResult<T> {
     case serverErr                  // 서버의 내부적 에러
     case networkFail                // 네트워크 연결 실패
 }
+
+public enum NetworkResult2<T> {
+    case success(T)                 // 서버 통신 성공
+    case requestErr              // 요청 에러 발생
+    case pathErr                    // 경로 에러
+    case serverErr                  // 서버의 내부적 에러
+    case networkFail                // 네트워크 연결 실패
+}

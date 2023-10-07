5a980393ce50d8a47f0d641ba30b04a6565f8564
hyun99999 <ypsophila97@icloud.com>
2023-10-07 10:25:20 +0900

    feat: SendTagCVC UI 및 레이아웃 구현 (#616)

--- NADA-iOS-forRelease/Sources/Cells/Tag/SendTagCVC.swift
+++ NADA-iOS-forRelease/Sources/Cells/Tag/SendTagCVC.swift
@@ -10,4 +10,50 @@ import UIKit
 import SnapKit
 
 class SendTagCVC: UICollectionViewCell {
+    
+    // MARK: - Components
+    
+    private let bgView = UIView()
+    private let checkImageView = UIImageView()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUI()
+        setLayout()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            checkImageView.isHidden = !isSelected
+        }
+    }
+}
+
+// MARK: - Extension
+
+extension SendTagCVC {
+    private func setUI() {
+        checkImageView.image = UIImage(named: "icnOptionCheck")
+        bgView.layer.cornerRadius = 16
+        checkImageView.isHidden = true
+    }
+    private func setLayout() {
+        contentView.addSubviews([bgView, checkImageView])
+        
+        bgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        checkImageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+}
 }

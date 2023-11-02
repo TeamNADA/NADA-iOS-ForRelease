15c23c12ac73b3e6e6bc1ff264bbbab88f3522b0
hyun99999 <ypsophila97@icloud.com>
2023-11-02 22:11:40 +0900

    feat: Color 추가 및 수정 (#632)

--- Widgets/Resource/Extension/Color+Extension.swift
+++ Widgets/Resource/Extension/Color+Extension.swift
@@ -19,6 +19,14 @@ extension Color {
         }
     }
     
+    static func primaryColor(for colorScheme: ColorScheme) -> Color {
+        if colorScheme == .light {
+            return Color(red: 19.0 / 255.0, green: 20.0 / 255.0, blue: 22.0 / 255.0)
+        } else {
+            return Color(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0)
+        }
+    }
+    
     static func userNameColor(for colorScheme: ColorScheme) -> Color {
         switch colorScheme {
         case .light:
@@ -30,11 +38,11 @@ extension Color {
         }
     }
     
-    static func secondaryColor(for colorScheme: ColorScheme) -> Color {
+    static func widgetBackgrounColor(for colorScheme: ColorScheme) -> Color {
         if colorScheme == .light {
-            return Color(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0)
-        } else {
             return Color(red: 241.0 / 255.0, green: 243.0 / 255.0, blue: 245.0 / 255.0)
+        } else {
+            return Color(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 49.0 / 255.0)
         }
     }
 }
